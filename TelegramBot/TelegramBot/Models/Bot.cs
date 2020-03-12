b4739399ae58bd6cbbdf17c2944b4e3dc2bbbf9b
Oleh Sytnyk <olexmessi@gmail.com>
2020-03-12 19:51:09 +0200

    Add appsettings and bot class.

---
+++ TelegramBot/TelegramBot/Models/Bot.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+
+
+namespace TelegramBot.Models
+{
+    public class Bot
+    {
+        private static TelegramBotClient client;
+
+        public async Task<TelegramBotClient> Get()
+        {
+            if (client != null)
+            {
+                return client;
+            }
+
+            client = new TelegramBotClient(AppSettings.Key);
+            await client.SetWebhookAsync("");
+
+            return client;
+        }
+    }
+}
