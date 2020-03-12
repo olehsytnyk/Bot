1fcd4fbb5609615ad4f93c5fe5b37ebafeb8728b
Oleh Sytnyk <olexmessi@gmail.com>
2020-03-12 21:47:14 +0200

    First command.

--- TelegramBot/TelegramBot/Models/Command/Command.cs
+++ TelegramBot/TelegramBot/Models/Command/Command.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Telegram.Bot.Types;
+﻿using Telegram.Bot.Types;
 using Telegram.Bot;
 
 namespace TelegramBot.Models.Command
